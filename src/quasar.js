0e4188c023f37251f4222c98a1ead07c9984862e
Ryle Capul <kencap119@gmail.com>
2021-04-21 22:09:49 +0800

    [1] 1.0 initial commit

---
+++ src/quasar.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+
+import './styles/quasar.sass'
+import iconSet from 'quasar/icon-set/material-icons-round.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {
+  },
+  iconSet: iconSet
+ })
